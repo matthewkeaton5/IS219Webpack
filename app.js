996fd8225c617848f56b7c90a253ea52df3cea8a
matthewkeaton5 <56092226+matthewkeaton5@users.noreply.github.com>
2021-05-05 17:27:17 -0400

    Added: Setup to follow guide, Installation of new requirements

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
-const open = require('open');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
+
 app.use(cors());
 app.use(express.static('docs'));
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -13,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // define a root route
 // Require employee routes
-const citiesRoutes = require('./routes/cities.routes');
+
 // using as middleware
 app.use('/api/v1/cities', citiesRoutes);
 app.set('port', process.env.PORT || 8000);
