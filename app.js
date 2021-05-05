3f9d5103068e4fa54cf610a47f25c4ab34b6e4c8
matthewkeaton5 <56092226+matthewkeaton5@users.noreply.github.com>
2021-05-05 17:29:44 -0400

    Added: app.js code from guide

--- app.js
+++ app.js
@@ -4,20 +4,18 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(cors());
-app.use(express.static('docs'));
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-// define a root route
-// Require employee routes
 
-// using as middleware
-app.use('/api/v1/cities', citiesRoutes);
-app.set('port', process.env.PORT || 8000);
-app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
-app.listen(app.get('port'), () => {
-    console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
-    open('http://localhost:8000');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+// Our requests hadlers will be implemented here...
+
+app.listen(3000);
