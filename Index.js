da6e2821a90308676939e098fae7c5bde7b16eef
matthewkeaton5 <56092226+matthewkeaton5@users.noreply.github.com>
2021-05-10 21:54:14 -0400

    Task: Connected pug

--- Index.js
+++ Index.js
@@ -10,3 +10,10 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
