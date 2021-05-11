96972ee5dbd7d0c1c3ff0d4ff71d489dc59ed525
matthewkeaton5 <56092226+matthewkeaton5@users.noreply.github.com>
2021-05-10 22:16:10 -0400

    Task: Auth strategy and imports

--- Index.js
+++ Index.js
@@ -1,7 +1,37 @@
 const express = require("express");
 const path = require("path");
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
 const app = express();
 const port = process.env.PORT || "8000";
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile);
+    }
+);
+
+require("dotenv").config();
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
 
 app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
@@ -13,7 +43,11 @@ app.listen(port, () => {
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
 });
+
