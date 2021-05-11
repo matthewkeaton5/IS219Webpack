4df118f5f3b354e2867f0e8c8fcddca994451ecb
matthewkeaton5 <56092226+matthewkeaton5@users.noreply.github.com>
2021-05-10 22:02:12 -0400

    Task: Added user.pug

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
     res.status(200).send("WHATABYTE: Food For Devs");
 });
 
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
+
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
 });
@@ -12,3 +16,4 @@ app.listen(port, () => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
